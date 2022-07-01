fe67ab358ffd2e57661ce3dcdbf4ec1447821dbe
zhkafake <zhkafake@icloud.com>
2022-07-01 11:26:33 +0300

    show temp data

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 function displayTemperature(response) {
   let humidity = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#wind-speed");
-  let units = "metric";
   let temperature = document.querySelector("#tempo");
-  let temperatureData = response.data.main.temp;
   let actualtemp = Math.round(response.data.main.temp);
   let windData = Math.round(response.data.wind.speed);
   let humidData = Math.round(response.data.main.humidity);
+  let wheatherConditions = document.querySelector("#condition");
+  let wheatherData = response.data.weather[0].description;
+  let city = document.querySelector("#picked-city");
+  let countryElement = document.querySelector("#country");
+  city.innerHTML = response.data.name;
+  countryElement.innerHTML = response.data.sys.country;
+  wheatherConditions.innerHTML = wheatherData;
   humidity.innerHTML = humidData;
   windSpeed.innerHTML = windData;
   temperature.innerHTML = actualtemp;
   axios.get(apiUrl).then(displayTemperature);
 }
-let city = "New York";
-let apiKey = "46ded6c2ff1abcc5f9d00793c7ce4966";
+let city = "Kyiv";
+let apiKey = "f9dcd16921b5c743196e0ded07686e68";
 let units = "metric";
 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
